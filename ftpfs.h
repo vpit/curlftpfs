44344249992cb48a30876729889189d8f35ac5d2
Vincent Pit <vince@profvince.com>
2015-07-16 12:15:56 -0300

    ftpfs.h: constify two struct members

--- ftpfs.h
+++ ftpfs.h
@@ -73,8 +73,8 @@ struct ftpfs {
   curl_version_info_data* curl_version;
   int safe_nobody;
   int tryutf8;
-  char *codepage;
-  char *iocharset;
+  const char *codepage;
+  const char *iocharset;
   int multiconn;
 };
 
